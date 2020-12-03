33ca7b7f4e5980ff0c1c04a0a8346aaf5de7ff16
Ro-Nel <rodrigo.saravia@ucb.edu.bo>
2020-12-03 19:23:27 -0400

    Login

--- app/src/main/java/com/example/proyectoandroid/RegistroActivity.java
+++ app/src/main/java/com/example/proyectoandroid/RegistroActivity.java
@@ -58,7 +58,7 @@ public class RegistroActivity extends AppCompatActivity {
     private void registrarCliente() throws ParseException {
 
 //"1992-10-10",
-        Date Nacimiento = Date.valueOf(txNacimiento.getText().toString());
+      //  Date Nacimiento = Date.valueOf(txNacimiento.getText().toString());
 
         String Name = txName.getText().toString();
         String Apellido = txApellido.getText().toString();
