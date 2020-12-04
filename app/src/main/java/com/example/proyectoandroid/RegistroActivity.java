4f5c3f890c7931475d5e0e4ebb1c73dbe80270da
Ro-Nel <rodrigo.saravia@ucb.edu.bo>
2020-12-04 18:09:05 -0400

    Calendario listo

--- app/src/main/java/com/example/proyectoandroid/RegistroActivity.java
+++ app/src/main/java/com/example/proyectoandroid/RegistroActivity.java
@@ -1,5 +1,7 @@
 package com.example.proyectoandroid;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -16,6 +18,7 @@ import java.sql.Date;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class RegistroActivity extends AppCompatActivity {
@@ -29,7 +32,13 @@ public class RegistroActivity extends AppCompatActivity {
     EditText txEmail;
     EditText txPassword;
     Button btRegistro;
+    Button btCalendario;
 
+    int anio=0,ani=0;
+    int mes=0,me=0;
+    int dia=0,di=0;
+    static final int DATE_ID = 0;
+    Calendar c =Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +50,25 @@ public class RegistroActivity extends AppCompatActivity {
         txEmail = (EditText) findViewById(R.id.email);
         txPassword = (EditText) findViewById(R.id.password);
         btRegistro = (Button) findViewById(R.id.registro);
+        //btCalendario = (Button) findViewById(R.id.calendario);
+        mes = c.get(Calendar.MONDAY);
+        dia = c.get(Calendar.DAY_OF_MONTH);
+        anio = c.get(Calendar.YEAR);
+
+        txNacimiento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DATE_ID);
+                //showDialog(DATE_ID);
+            }
+        });
+
+        /*btCalendario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                obtenerFecha();
+            }
+        });*/
 
 
         btRegistro.setOnClickListener(new View.OnClickListener() {
@@ -54,8 +82,60 @@ public class RegistroActivity extends AppCompatActivity {
             }
         });
     }
+    private void colocar_fecha() {
+        txNacimiento.setText((me + 1) + "-" + di + "-" + ani+" ");
+    }
 
 
+
+    private DatePickerDialog.OnDateSetListener mDateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    ani = year;
+                    me = monthOfYear;
+                    di = dayOfMonth;
+                    colocar_fecha();
+
+                }
+
+            };
+
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_ID:
+                return new DatePickerDialog(this, mDateSetListener, anio, mes, dia);
+
+
+        }
+
+
+        return null;
+    }
+
+
+
+
+/*    @Override
+    public void onClick(View v) {
+        obtenerFecha();
+    }*/
+    /*private void obtenerFecha() {
+        final DatePickerDialog recogerFecha = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            //final DatePickerDialog recogerFecha = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            //final DatePickerDialog recogerFecha = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int año, int mesi, int diaa) {
+                final int mesActual = mesi + 1;
+                String diaFormateado = (diaa < 10) ? "0" + String.valueOf(diaa) : String.valueOf(diaa);
+                String mesFormateado = (mesActual < 10) ? "0" + String.valueOf(mesActual) : String.valueOf(mesActual);
+                txNacimiento.setText(diaFormateado + "-" + mesFormateado + "-" + año);
+            }
+        }, anio, mes, dia);recogerFecha.getDatePicker().setMinDate(30);
+        recogerFecha.getDatePicker().setMaxDate(30);
+        recogerFecha.show();
+    }*/
 
     private void registrarCliente() throws ParseException {
 
